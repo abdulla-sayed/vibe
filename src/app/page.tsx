94e0e9eb2fe9791261bee461292b1ba22f941337
Dev-Abood <abdullasayed10@gmail.com>
2026-08-19 22:20:18 +0300

    initial setup commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <div>
+      <Button variant="destructive">Click here</Button>
+    </div>
+  );
+}
